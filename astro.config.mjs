500eb2a0efd911394519e307d6393cf5b59340cb
alexander muñoz <alexander12345my@gmail.com>
2023-09-17 14:19:07 -0500

    blog jamy

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  viewTransitions: true,
+  site: "https://MINOTAURO18.github.io",
+  base: "/blog",
+});
---
+++ src/data/menuItemsData.ts
@@ -0,0 +1,57 @@
+import type { MenuItem } from '../types/types';
+
+const menuItems: MenuItem[]  = [
+    {
+        title : 'Home',
+        url: '/blog',
+        
+        
+    },
+    {
+        title : 'HTML',
+        url: '/blog/html',
+    },
+    {
+        title : 'CSS',
+        url: '/blog/css',
+    },
+    {
+        title : 'Tailwind',
+        url: '/blog/tailwind',
+    },
+    {
+        title : 'Javascript',
+        url: '/blog/javascript',
+    },
+    {
+        title : 'React',
+        url: '/blog/react',
+    },
+    {
+        title : 'Typescript',
+        url: '/blog/typescript',
+    },
+    {
+        title : 'Python',
+        url: '/blog/python',
+    },
+    {
+        title : 'Node',
+        url: '/blog/node',
+    },
+    {
+        title : 'Express',
+        url: '/blog/express',
+    },
+    {
+        title : 'Mongo',
+        url: '/blog/mongo',
+    },
+    {
+        title : 'SQL',
+        url: '/blog/sql',
+    },
+    
+]
+
+export default menuItems;
