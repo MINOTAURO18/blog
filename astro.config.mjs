a3408b1eaefd289f37d298422744b322e5d74fa8
alexander muñoz <alexander12345my@gmail.com>
2023-09-17 21:58:09 -0500

    animations 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
     viewTransitions: true,
-  },
   site: "https://MINOTAURO18.github.io",
   base: "/blog",
 });
